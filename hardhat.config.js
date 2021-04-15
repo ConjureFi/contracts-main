53eda6fc2cbbe0e45d4c196f19a6b1c41913b686
richance <33940018+richance@users.noreply.github.com>
2021-04-15 21:13:01 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
 require('hardhat-contract-sizer');
-require('dotenv').config()
 require("@nomiclabs/hardhat-etherscan");
 require("solidity-coverage");
 
@@ -14,9 +13,6 @@ module.exports = {
 
     // do a mainnet fork to work with oracle data
     hardhat: {
-      forking: {
-       url: "https://eth-mainnet.alchemyapi.io/v2/" + process.env.ALCHEMY_ID
-		}
 	}
   },
     etherscan: {
